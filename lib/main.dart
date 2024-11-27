b1714c3d2e8885438e30264516347cb4f5fe8e0b
Moataz AbdElwhab <MoatazAbdElwhab@Gmail.com>
2024-11-28 00:42:54 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/news/view_model/news_view_model.dart';
+import 'package:news_app/settings/view_model/settings_view_model.dart';
 import 'package:news_app/shared/app_theme.dart';
 import 'package:news_app/home/view/screens/home_screen.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(
-    ChangeNotifierProvider(
-      create: (context) => NewsViewModel(),
-      child: const NewsApp(),
-    ),
-  );
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(
+        create: (context) => NewsViewModel(),
+      ),
+      ChangeNotifierProvider(
+        create: (context) => SettingsViewModel(),
+      ),
+    ],
+    child: const NewsApp(),
+  ));
 }
 
 class NewsApp extends StatelessWidget {
